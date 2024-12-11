28246a153bea03b676d77ca315936015a62a7a4b
Giancarlo Kramer <92767138+ProgAttack@users.noreply.github.com>
2024-12-11 16:09:58 -0300

    finish challenge

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,6 @@ class _StoryPageState extends State<StoryPage> {
   StoryBrain storyBrain = StoryBrain();
   int storyNumber = 0;
 
- 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,7 +58,6 @@ class _StoryPageState extends State<StoryPage> {
                     setState(() {
                       storyBrain.nextStory(1);
                     });
-                    //*storyBrain.nextStory(1);
                   },
                   style: ButtonStyle(
                       backgroundColor: WidgetStateProperty.all(Colors.red)),
@@ -77,8 +74,6 @@ class _StoryPageState extends State<StoryPage> {
               ),
               Expanded(
                 flex: 2,
-                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
-                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
                 child: Visibility(
                   visible: storyBrain.buttonShouldBeVisible(),
                   child: TextButton(
@@ -86,7 +81,6 @@ class _StoryPageState extends State<StoryPage> {
                       setState(() {
                         storyBrain.nextStory(2);
                       });
-                      
                     },
                     style: ButtonStyle(
                         backgroundColor: WidgetStateProperty.all(Colors.blue)),
@@ -106,7 +100,3 @@ class _StoryPageState extends State<StoryPage> {
     );
   }
 }
-
-//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
-
-//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
